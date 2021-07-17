99f7fb47bba47f68f57d8c0f7a2959b76c8bbfcc
Adam Chyła <adam@chyla.org>
2021-07-18 00:43:20 +0200

    update year in copyright text

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020, Adam Chyła <adam@chyla.org>.
+ * Copyright (c) 2019-2021, Adam Chyła <adam@chyla.org>.
  * All rights reserved.
  *
  * Distributed under the terms of the BSD 3-Clause License.
@@ -27,7 +27,7 @@ typedef std::vector<Path> TestPaths;
 
 const char * const license_text = R"(BSD 3-Clause License
 
-Copyright (c) 2019-2020, Adam Chyła <adam@chyla.org>
+Copyright (c) 2019-2021, Adam Chyła <adam@chyla.org>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -117,7 +117,7 @@ main(int argc, char **argv)
 
         if (vm.count("version")) {
             std::cout << argv[0] << " " << VERSION << '\n'
-                      << "Copyright (c) 2019-2020, Adam Chyła <adam@chyla.org>.\n"
+                      << "Copyright (c) 2019-2021, Adam Chyła <adam@chyla.org>.\n"
                       << "All rights reserved.\n"
                       << "\n"
                       << "Distributed under the terms of the BSD 3-Clause License.\n";
--- systemtests/common/test_TestExecutionMatchers.py
+++ systemtests/common/test_TestExecutionMatchers.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2020, Adam Chyła <adam@chyla.org>.
+# Copyright (c) 2019-2021, Adam Chyła <adam@chyla.org>.
 # All rights reserved.
 #
 # Distributed under the terms of the BSD 3-Clause License.
