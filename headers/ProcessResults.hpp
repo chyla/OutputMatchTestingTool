b6eccc629dd4b84f533637202a24f34b1f85a559
Adam Chyła <adam@chyla.org>
2025-01-13 12:53:15 +0100

    update copyright year

--- headers/ProcessResults.hpp
+++ headers/ProcessResults.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2024, Adam Chyła <adam@chyla.org>.
+ * Copyright (c) 2019-2025, Adam Chyła <adam@chyla.org>.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
